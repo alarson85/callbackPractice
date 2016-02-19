c8b1d99408209054f8cb9666f4c14bdda6d856b5
Alex Larson <alarson85@gmail.com>
2016-02-19 14:14:23 -0600

    pushing everything to github, this still needs work

--- callbackPractice.js
+++ callbackPractice.js
@@ -3,6 +3,8 @@ Below is a sample problem
 
   //code here for sayHi
 
+ 
+
    sayHi('Hi Katie', function(thingToSay){
       alert(thingToSay);
    });
@@ -25,7 +27,13 @@ and what you should write is the favNum function that makes the code above work,
 
 
   //Code Here for first
-  
+
+
+function first(arr, cb) {
+    cb(arr[0]);
+}
+
+
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 first(names, function(firstName){
   console.log('The first name in names is ', firstName)
@@ -39,7 +47,9 @@ first(names, function(firstName){
 
 
   //Code Here for last
-
+function last(arr, cb) {
+    cb(arr[arr.length - 1]);
+};
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 last(names, function(lastName){
   console.log('The last name in names is ', lastName);
@@ -58,6 +68,10 @@ last(names, function(lastName){
 
   //Code Here for multiply
 
+function multiply(numX, numY, cb) {
+  cb(numX * numY);
+}
+
 multiply(4, 3, function(answer){
   console.log('The answer is ', answer); //should console.log 12
 })
@@ -73,6 +87,25 @@ multiply(4, 3, function(answer){
 
 
   //Code Here for contains
+var contains = function(arr, target, cb){
+  var found = false;
+  if (arr.indexOf(target) !== -1){
+    found = true;
+  }
+  cb(found);
+}
+
+//how does indexOf() work, why !== -1, why do we start by setting the var to false?
+
+function contains(arr, name, cb) {
+  var found = false;
+  for(var i = 0; i<arr.length; i++){
+    if(arr[i] === name){
+      found = true
+    }
+  } 
+  cb(found);
+}
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 contains(names, 'Colt', function(result){
@@ -94,6 +127,52 @@ contains(names, 'Colt', function(result){
 
     //Code Here for uniq
 
+
+function uniq(arr, cb){
+  var newArr = [];
+  arr.indexOf()
+}
+
+
+
+function uniq(arr, cb){
+  var uniqArray = [];
+  for(var i = 0; i<arr.length; i++){
+    for(var i = 0; i<arr.length; i++)
+      if(arr[i] === arr[i] && i !== i){
+        uniqArray = arr[i].splice(i,1);
+      }
+  }
+  cb(uniqArray);
+}
+
+function uniq(arr, cb){
+     var newArr = arr.filter(function(item){
+       if(arr.indexOf(item) === arr.lastIndexOf(item)){
+         return true;
+       }
+     });
+     console.log(newArr)
+     cb(newArr)
+
+
+    }
+
+    //Code Here for uniq
+    //creates a new object, then sets each value equal to true, the array won't add properties that are the same. Then loops through and pushes that array to uniqArray
+    function uniq(names, cb) {
+      var obj = {};
+      var uniqArray = [];
+      for (var i = 0; i < names.length; i++) {
+        obj[names[i]] = true;
+      }
+      for (var key in obj) {
+        uniqArray.push(key);
+      }
+      cb(uniqArray);
+    }
+
+
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
@@ -109,6 +188,11 @@ uniq(names, function(uniqArr){
 
 
     //Code Here for each
+    var each = function (arr, cb) {
+      return arr.map(function(item, index) {
+       cb(item, index);
+      })
+    }
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 each(names, function(item, indice){
@@ -126,6 +210,17 @@ each(names, function(item, indice){
 
 
  //code here for getUserById
+ var getElementsByTagName = function(object, id, cb) {
+
+ }
+
+ var getUserById = function(arr, id, cb) {
+  arr.map(function(obj, index) {
+    if(obj.id === id) {
+      cb(obj);
+    }
+  });
+}
 
 var users = [
   {
